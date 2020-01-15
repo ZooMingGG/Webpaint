e046099fe8f14f2dfc7ebb22e7a4045768865112
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-01-15 15:57:21 +0200

    did a js part of toggle button nav

---
+++ js/index.js
@@ -0,0 +1,5 @@
+'use strict'
+function showNaw(){
+    let toggle = document.getElementById("toggle").classList.toggle('toggle-active');
+    let nav = document.getElementById("nav").classList.toggle('nav-active');
+  };
